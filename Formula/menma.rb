f0454977ece296a7e890ff3b48ab7d86a20f5e2d
Ryota KITA <snptkdn@snptkdn-3.local>
2024-01-18 22:39:40 +0900

    add: add v0.1.0

---
+++ Formula/menma.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Menma < Formula
+  desc "menma is memo and manager"
+  homepage "https://github.com/snptkdn/menma"
+  url "https://github.com/snptkdn/menma/archive/v0.1.0.tar.gz"
+  sha256 "3375cc44a65a963f0766f107386117e015dfaeedfd422f6f76a0320c7c413d6f"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test menma`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
